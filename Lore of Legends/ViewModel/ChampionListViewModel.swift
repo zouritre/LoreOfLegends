63f1822b2399618d430aa9749f78e9b65c9a10d4
zouritre <timouf97400@live.fr>
2022-10-16 16:54:02 +0200

    Fix file path referencing directory outside current project

---
+++ Lore of Legends/ViewModel/ChampionListViewModel.swift
@@ -0,0 +1,59 @@
+//
+//  ChampionListViewModel.swift
+//  LoreOL
+//
+//  Created by Bertrand Dalleau on 12/10/2022.
+//
+
+import Foundation
+
+protocol ChampionListViewModelDelegate {
+    func championList(champions: [Champion]?, error: Error?)
+}
+
+class ChampionListViewModel {
+    var champions: [Champion]? {
+        willSet {
+            delegate?.championList(champions: newValue, error: nil)
+        }
+    }
+    
+    var championsDataError: Error? {
+        willSet {
+            delegate?.championList(champions: nil, error: newValue)
+        }
+    }
+    
+    var api: ChampionListDelegate
+    var delegate: ChampionListViewModelDelegate?
+    var championListModel = ChampionList()
+
+    init(api: ChampionListDelegate = ChampionListApi()) {
+        self.champions = []
+        self.api = api
+        
+        let championListListenerName = Notification.Name("championsList")
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(listenChampionsList(_:)), name: championListListenerName, object: nil)
+    }
+    
+    @objc func listenChampionsList(_ sender: Notification) {
+        guard let sender = sender.userInfo else {
+            championsDataError = ChampionListError.NotificationDataIsEmpty
+            return
+        }
+        
+        if let champions = sender["list"] as? [Champion] {
+            self.champions = champions
+        } else {
+            championsDataError = ChampionListError.NotificationDataBadType
+            return
+        }
+        
+    }
+    
+    func getChampions() {
+        championListModel.delegate = api
+        championListModel.delegate?.getChampions()
+    }
+}
