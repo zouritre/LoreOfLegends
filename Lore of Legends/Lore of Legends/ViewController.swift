6183b726865a6b9c562ec5b41b3671170ba3c6ed
zouritre <timouf97400@live.fr>
2022-10-16 13:39:55 +0200

    First commit

---
+++ Lore of Legends/Lore of Legends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lore of Legends
+//
+//  Created by Bertrand Dalleau on 16/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
