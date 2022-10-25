37b2bbdee43b707edf40dd5d61ea1e422bf5af0c
zouritre <timouf97400@live.fr>
2022-10-25 19:39:18 +0200

    ChampionList: Add comments

--- Lore of Legends/Model/ChampionList/ChampionList.swift
+++ Lore of Legends/Model/ChampionList/ChampionList.swift
@@ -8,11 +8,17 @@
 import Foundation
 import Combine
 
+/// Protocol to wich conform a class that's responsible for requesting full champion list and their icons
 protocol ChampionListDelegate {
+    /// Request the champion list and set their icons
+    /// - Parameter caller: Model responsible for sending the API data to the view-model
     func getChampions(_ caller: ChampionList)
 }
 
+/// Model class that manages request for the champion list API
 class ChampionList {
+    /// Delegate that manage the API request
     var delegate: ChampionListDelegate?
+    /// Publisher that send the API data back to the view-model subscribers
     var championsDataSubject = PassthroughSubject<[Champion], Error>()
 }
