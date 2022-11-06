292923e4319d9f52bb84543958231be0bf7816f1
zouritre <timouf97400@live.fr>
2022-11-06 17:45:58 +0100

    ChampionDetailAdapter: Add comments

--- Lore of Legends/Model/ChampionDetail/ChampionDetailAdapter.swift
+++ Lore of Legends/Model/ChampionDetail/ChampionDetailAdapter.swift
@@ -8,10 +8,18 @@
 import Foundation
 
 protocol ChampionDetailAdapterDelegate {
+    /// Retrieve and set skins images for the given champion
+    /// - Parameters:
+    ///   - caller: The class calling this method
+    ///   - champion: Champion object for wich to retrieve skins images
     func setSkins(caller: ChampionDetailAdapter, for champion: Champion)
 }
 
 extension ChampionDetailAdapter: ChampionDetailDelegate {
+    /// Retrieve and set skins images for the given champion
+    /// - Parameters:
+    ///   - caller: The class calling this method
+    ///   - champion: Champion object for wich to retrieve skins images
     func setSkinImages(caller: ChampionDetail, champion: Champion) {
         self.caller = caller
         delegate.setSkins(caller: self, for: champion)
@@ -19,7 +27,9 @@ extension ChampionDetailAdapter: ChampionDetailDelegate {
 }
 
 class ChampionDetailAdapter {
+    /// Class responsible for making async tasks
     var delegate: ChampionDetailAdapterDelegate
+    /// Class wich called method of this instance
     var caller: ChampionDetail?
     
     init(delegate: ChampionDetailAdapterDelegate = RiotCdnApi()) {
