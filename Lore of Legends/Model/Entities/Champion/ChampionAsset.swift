4d1cf6481222e8607afbcee21eef013bfe835853
zouritre <timouf97400@live.fr>
2022-11-06 18:15:08 +0100

    ChampionAsset: Add comments

--- Lore of Legends/Model/Entities/Champion/ChampionAsset.swift
+++ Lore of Legends/Model/Entities/Champion/ChampionAsset.swift
@@ -7,16 +7,25 @@
 
 import Foundation
 
+/// Store information about a skin for a given champion
 struct ChampionAsset: Codable {
+    /// File name for the skin
     var fileName: String
+    /// Skin name
     var title: String
+    /// Splash image as data object
     var splash: Data?
+    /// Centered image as data object
     var centered: Data?
     
+    /// Set the splash property with the provided data object
+    /// - Parameter data: Data object to be set to the local property
     mutating func setSplash(with data: Data?) {
         self.splash = data
     }
     
+    /// Set the centered property with the provided data object
+    /// - Parameter data: Data object to be set to the local property
     mutating func setCenteredImage(with data: Data?) {
         self.centered = data
     }
