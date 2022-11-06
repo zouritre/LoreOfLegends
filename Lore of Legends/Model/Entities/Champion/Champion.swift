3eed00d9c9ccb9c702629b5a7e2fba6b902ff46a
zouritre <timouf97400@live.fr>
2022-11-06 18:10:06 +0100

    Champion: Add comments

--- Lore of Legends/Model/Entities/Champion/Champion.swift
+++ Lore of Legends/Model/Entities/Champion/Champion.swift
@@ -7,14 +7,23 @@
 
 import Foundation
 
+/// Store information about a given champion in League
 struct Champion: Codable {
+    /// Champion name
     var name: String
+    /// Champion honorific name
     var title: String
+    /// The skins images name for this champion according to Riot CDN
     var imageName: String
+    /// Champion icon as a data object
     var icon: Data = Data()
+    /// Array repsenting every skins of this champion
     var skins: [ChampionAsset]
+    /// Lore of this champion
     var lore: String
     
+    /// Set the given data object to the icon property of this class
+    /// - Parameter data: Data object representing this champion icon
     mutating func setIcon(with data: Data) {
         self.icon = data
     }
