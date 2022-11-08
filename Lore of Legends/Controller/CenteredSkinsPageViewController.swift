99f9c62d4c1d7705e81b7fc3717f1bee3a5b9c73
zouritre <timouf97400@live.fr>
2022-11-08 14:52:27 +0100

    Add UIPageViewController

---
+++ Lore of Legends/Controller/CenteredSkinsPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CenteredSkinsPageViewController.swift
+//  Lore-of-Legends
+//
+//  Created by Bertrand Dalleau on 08/11/2022.
+//
+
+import UIKit
+
+class CenteredSkinsPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
