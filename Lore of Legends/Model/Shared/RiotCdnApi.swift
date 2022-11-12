7c845e6b469709348ee51a8d6e894b17562902f3
zouritre <timouf97400@live.fr>
2022-11-12 02:09:19 +0100

    Send download icons progress

--- Lore of Legends/Model/Shared/RiotCdnApi.swift
+++ Lore of Legends/Model/Shared/RiotCdnApi.swift
@@ -17,7 +17,7 @@ extension RiotCdnApi: RiotCdnApiDelegate {
         
         // Group every champion icon download in a task group and stop execution until they finish
         let champions = await withTaskGroup(of: Champion.self) { taskGroup in
-            for (_, info) in decodable.data {
+            for (index, (_, info)) in decodable.data.enumerated() {
                 // Icon name
                 var imageName = info.image.full
                 
@@ -31,6 +31,9 @@ extension RiotCdnApi: RiotCdnApiDelegate {
                     // Get icon as data object from url
                     let data = try? await getData(at: url)
                     let champion = Champion(name: info.name, title: "", imageName: "", icon: data, skins: [], lore: "")
+                    let nonZeroBasedIndex = index+1
+                    
+                    caller.iconsDownloadedPublisher.send(nonZeroBasedIndex)
                     
                     return champion
                 }
