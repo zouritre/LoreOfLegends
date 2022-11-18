9e8855a82b05e1736a7d20e578714dc699d361b6
zouritre <timouf97400@live.fr>
2022-11-18 19:17:06 +0100

    Add UIImageView to UIScrollViewe

--- Lore of Legends/Controller/SkinViewController/SkinViewController.swift
+++ Lore of Legends/Controller/SkinViewController/SkinViewController.swift
@@ -16,7 +16,6 @@ class SkinViewController: UIViewController {
     
     @IBOutlet weak var skinImageView: UIImageView!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
