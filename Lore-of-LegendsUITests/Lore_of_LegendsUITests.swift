36e6ea0b4e8fdfb645c9258bb541737b136b83cc
zouritre <timouf97400@live.fr>
2022-11-27 00:48:12 +0100

    Finish UI tests for homescreen, patch notes and language selection

--- Lore-of-LegendsUITests/Lore_of_LegendsUITests.swift
+++ Lore-of-LegendsUITests/Lore_of_LegendsUITests.swift
@@ -10,6 +10,7 @@ import XCTest
 
 final class Lore_of_LegendsUITests: XCTestCase {
     var app: XCUIApplication!
+    var navigationBar: XCUIElement!
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -22,6 +23,10 @@ final class Lore_of_LegendsUITests: XCTestCase {
         app = XCUIApplication()
         setupSnapshot(app)
         app.launch()
+        
+        // Wait Homescreen to appear after downloading champions
+        navigationBar = app.navigationBars["Lore of Legends"].buttons.element(boundBy: 0)
+        XCTAssertTrue(navigationBar.waitForExistence(timeout: 30))
     }
 
     override func tearDownWithError() throws {
@@ -33,37 +38,49 @@ final class Lore_of_LegendsUITests: XCTestCase {
     }
     
     func testChooseLanguageForLores() {
-        app.navigationBars["Lore of Legends"].buttons["Item"].tap()
-        let pickerExist = app.pickerWheels["ANGLAIS"].waitForExistence(timeout: 5)
-        XCTAssertTrue(pickerExist)
+        let settingsButton = app.navigationBars["Lore of Legends"].buttons.element(boundBy: 0)
+        // Navigate to settings
+        settingsButton.tap()
+        
+        let languagePicker = app.pickerWheels.element(boundBy: 0)
+        // Wait for async query to get languages
+        XCTAssertTrue(languagePicker.waitForExistence(timeout: 5))
+        
+        // Snapshot !
         snapshot("02LanguagePicker")
-        app.pickerWheels["ANGLAIS"].tap()
-        app/*@START_MENU_TOKEN@*/.staticTexts["Sauvegarder"]/*[[".buttons[\"Sauvegarder\"].staticTexts[\"Sauvegarder\"]",".staticTexts[\"Sauvegarder\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.alerts["Information"].scrollViews.otherElements.buttons["Ok"].tap()
-        app.navigationBars["Lore_of_Legends.SettingsView"].buttons["Lore of Legends"].tap()
-                
+        
+        // Save settings button
+        let saveButton = app.buttons.matching(identifier: "SaveSettingsButton").element
+        saveButton.tap()
+        // Tap Ok button from alert
+        app.alerts.element(boundBy: 0).scrollViews.otherElements.buttons.element(boundBy: 0).tap()
+        // Tap back button from nav bar
+        app.navigationBars["Lore_of_Legends.SettingsView"].buttons.element(boundBy: 0).tap()
     }
     
     func testDisplayPatchNotes() {
-        
-        let app = XCUIApplication()
-        let patchNoteButton = app/*@START_MENU_TOKEN@*/.toolbars["Toolbar"]/*[[".toolbars[\"Barre d’outils\"]",".toolbars[\"Toolbar\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.buttons["Version de patch: 12.22.1"]
-        XCTAssertTrue(patchNoteButton.waitForExistence(timeout: 10))
+        let patchNoteButton = app.toolbars.element(boundBy: 0).buttons.element(boundBy: 0)
+        // Navigate to patch note webview
         patchNoteButton.tap()
+        
         let backButton = app/*@START_MENU_TOKEN@*/.otherElements["URL"]/*[[".otherElements[\"BrowserView?IsPageLoaded=true&WebViewProcessID=54115\"]",".otherElements[\"TopBrowserBar\"]",".buttons[\"Adresse\"]",".otherElements[\"Adresse\"]",".otherElements[\"URL\"]",".buttons[\"URL\"]"],[[[-1,4],[-1,3],[-1,5,3],[-1,2,3],[-1,1,2],[-1,0,1]],[[-1,4],[-1,3],[-1,5,3],[-1,2,3],[-1,1,2]],[[-1,4],[-1,3],[-1,5,3],[-1,2,3]],[[-1,4],[-1,3]]],[0]]@END_MENU_TOKEN@*/
         
-        XCTAssertTrue(backButton.waitForExistence(timeout: 5))
+        // Wait webpage to load
+        XCTAssertTrue(backButton.waitForExistence(timeout: 10))
         
+        // Snapshot !
         snapshot("03PatchNotes")
+        
+        // Return to homescreen
         backButton.tap()
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+//    func testLaunchPerformance() throws {
+//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+//            // This measures how long it takes to launch your application.
+//            measure(metrics: [XCTApplicationLaunchMetric()]) {
+//                XCUIApplication().launch()
+//            }
+//        }
+//    }
 }
